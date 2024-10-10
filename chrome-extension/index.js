f2e3aba477fdb7cf2f0e7843d337c39b6adc1962
BvB-tech <138557344+BvB-tech@users.noreply.github.com>
2024-10-10 11:06:19 +0200

    donderdag-1010-2024

---
+++ chrome-extension/index.js
@@ -0,0 +1,9 @@
+let inputBtn = document.getElementById('input-btn');
+
+inputBtn.addEventListener('click', function () {
+  console.log('button was clicked  addEventListener');
+});
+
+function saveLead() {
+  console.log('button was clicked onclick attribute');
+}
